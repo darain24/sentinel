6882d1c0efb06ac1302a06e9486ecc8622af7d2e
darain24 <darainqamar10@gmail.com>
2026-05-06 18:43:08 +0530

    fix: move rewrites to next.config and remove vercel.json to fix uv sync error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["react-leaflet", "leaflet"],
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/index.py",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
